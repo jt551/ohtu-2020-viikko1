845a3f7121be9821c2db0c15c0dc3111d6ff8c9a
juha t <jtwebapps@gmail.com>
2020-03-01 13:41:12 +0200

    teht 1.8 tehty

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -13,11 +13,21 @@ import static org.junit.Assert.*;
 public class VarastoTest {
 
     Varasto varasto;
+    Varasto varasto2;
+    Varasto varasto3;
+    Varasto varasto4;
+    Varasto varasto5;
+    Varasto varasto6;
     double vertailuTarkkuus = 0.0001;
 
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+        varasto2 = new Varasto(10, 11);
+        varasto3 = new Varasto(-5);
+        varasto4 = new Varasto(-5, 2);
+        varasto5 = new Varasto(5, -7);
+        varasto6 = new Varasto(10, 8);
     }
 
     @Test
@@ -28,6 +38,34 @@ public class VarastoTest {
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
         assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+    @Test
+
+    public void uudellaVarastollaOikeaTilavuusneg() {
+        assertEquals(0, varasto3.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uudellaVarastollaOikeaTilavuusJaSaldoKunYli() {
+        assertEquals(10, varasto2.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void uudellaVarastollaOikeaTilavuusJaSaldo() {
+        assertEquals(8, varasto6.getSaldo(), vertailuTarkkuus);
+    }
+    @Test
+
+    public void uudellaVarastollaOikeaTilavuusJaSaldoneg() {
+        assertEquals(0, varasto4.getTilavuus(), vertailuTarkkuus);
+    }
+    @Test
+    public void uudellaVarastollaOikeaSaldo() {
+        assertEquals(0, varasto5.getSaldo(), vertailuTarkkuus);
+    }
+    @Test
+    public void uudellaVarastollaOikeaVapaaTila() {
+        assertEquals(0, varasto2.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
     @Test
@@ -63,6 +101,39 @@ public class VarastoTest {
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisäysEiLisääTilavuutta() {
+        varasto.lisaaVarastoon(12);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisääNegatiivinen() {
+        varasto.lisaaVarastoon(5);
+        varasto.lisaaVarastoon(-2);
+        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void ottaminenEiOtaLiikaa() {
+        varasto.lisaaVarastoon(12);
+        double saatu = varasto.otaVarastosta(11);
+        assertEquals(10, saatu, vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaNegatiivinen() {
+        varasto.lisaaVarastoon(11);
+
+        varasto.otaVarastosta(-1);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    @Test
+    public void printSaldo() {
+        String expected = "saldo = 0.0, vielä tilaa 10.0";
+        assertEquals(expected, varasto.toString());
     }
 
 }
