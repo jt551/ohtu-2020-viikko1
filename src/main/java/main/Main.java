d04cb9eed9918884038b0889840bd7668eb3c045
jt551 <jtwebapps@gmail.com>
2020-03-23 15:35:59 +0200

    2.3

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,26 +12,5 @@ public class Main {
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Luonnin jälkeen:");
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Luonnin jälkeen:");
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Luonnin jälkeen:");
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Luonnin jälkeen:");
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Luonnin jälkeen:");
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Olutvarasto: " + olutta);
-        
     }
 }
