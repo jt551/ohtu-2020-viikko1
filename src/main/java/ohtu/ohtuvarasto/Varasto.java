468bb5816d2aad03bb80eb0a4ef8109a39295d88
jt551 <jtwebapps@gmail.com>
2020-03-23 14:09:51 +0200

    2.2

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -18,10 +18,11 @@ public class Varasto {
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+        
         if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
             if (alkuSaldo <= tilavuus) {
-                this.saldo = alkuSaldo;
+                this.saldo = Math.max(0,alkuSaldo);
             }else{
                 this.saldo = tilavuus;
             }
